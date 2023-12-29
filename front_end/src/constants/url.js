43ecdb6a5e2bcbde933a18c6027e71d7b658eab4
lemanhbin <pacmam2k1@gmail.com>
2023-12-29 22:08:48 +0700

    fix vercel

--- front_end/src/constants/url.js
+++ front_end/src/constants/url.js
@@ -1,2 +1,4 @@
-export const BE_URL = 'http://localhost:8080/api';
-export const IMAGE_URL = 'http://localhost:8080/image';
+// export const BE_URL = 'http://localhost:8080/api';
+// export const IMAGE_URL = 'http://localhost:8080/image';
+export const BE_URL = 'https://spa-godiva-server.vercel.app/api';
+export const IMAGE_URL = 'https://spa-godiva-server.vercel.app/image';
